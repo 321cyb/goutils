15af1398690356a65ff3264a317469ea40321504
Kevin Chen <321cyb@gmail.com>
2014-10-18 12:13:13 +0800

    modify log.

--- lvlog/log.go
+++ lvlog/log.go
@@ -3,11 +3,17 @@
 // messages below that level will actually be logged. For example, if
 // Level is set to LevelWarning, only log messages at the Warning,
 // Error, and Critical levels will be logged.
+
+// TODO: What if No Init function is called?
+
 package log
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	golog "log"
+	"os"
 )
 
 // The following constants represent logging levels in increasing levels of seriousness.
@@ -16,41 +22,89 @@ const (
 	LevelInfo
 	LevelWarning
 	LevelError
-	LevelCritical
 )
 
 var levelPrefix = [...]string{
-	LevelDebug:    "[DEBUG] ",
-	LevelInfo:     "[INFO] ",
-	LevelWarning:  "[WARNING] ",
-	LevelError:    "[ERROR] ",
-	LevelCritical: "[CRITICAL] ",
+	LevelDebug:   "[DEBUG] ",
+	LevelInfo:    "[INFO] ",
+	LevelWarning: "[WARNING] ",
+	LevelError:   "[ERROR] ",
 }
 
 // Level stores the current logging level.
-var Level = LevelDebug
+var level = LevelInfo
+var logFile = os.Stderr
+
+var levelString = "INFO"
+var filename = ""
+var logger golog.Logger
+
+func init() {
+	flag.StringVar(&levelString, "log-level", "INFO", "log level, can only be DEBUG, INFO, WARNING, ERROR")
+	flag.StringVar(&filename, "log-file", "", "log file name")
+}
+
+func setFileName(name string) error {
+	var err error
+	if len(name) > 0 {
+		logFile, err = os.OpenFile(name, os.O_RDWR, 0666)
+	}
+	golog.SetOutput(logFile)
+	golog.SetFlags(golog.LstdFlags | golog.Lshortfile)
+	return err
+}
+
+// InitFromArgs is the preferred way to init this library.
+func InitFromArgs() {
+	switch levelString {
+	case "DEBUG":
+		level = LevelDebug
+	case "INFO":
+		level = LevelInfo
+	case "WARNING":
+		level = LevelWarning
+	case "ERROR":
+		level = LevelError
+	default:
+		fmt.Fprintln(os.Stderr, "log-level can only be DEBUG, INFO, WARNING, ERROR")
+		os.Exit(1)
+	}
+	setFileName(filename)
+}
+
+// InitLevelAndFile is used to manually init this library
+func InitLevelAndFile(l int, p string) error {
+	if l <= LevelError && l >= LevelDebug {
+		level = l
+	} else {
+		return errors.New("wrong level number")
+	}
+	return setFileName(p)
+}
 
 func outputf(l int, format string, v []interface{}) {
-	if l >= Level {
+	if l >= level {
 		golog.Printf(fmt.Sprint(levelPrefix[l], format), v...)
 	}
 }
 
 func output(l int, v []interface{}) {
-	if l >= Level {
+	if l >= level {
 		golog.Print(levelPrefix[l], fmt.Sprint(v...))
 	}
 }
 
-// Criticalf logs a formatted message at the "critical" level. The
+// Fatalf logs a formatted message at the "critical" level. The
 // arguments are handled in the same manner as fmt.Printf.
-func Criticalf(format string, v ...interface{}) {
-	outputf(LevelCritical, format, v)
+func Fatalf(format string, v ...interface{}) {
+	outputf(LevelError, format, v)
+	os.Exit(1)
 }
 
-// Critical logs its arguments at the "critical" level.
-func Critical(v ...interface{}) {
-	output(LevelCritical, v)
+// Fatal logs its arguments at the "critical" level.
+func Fatal(v ...interface{}) {
+	output(LevelError, v)
+	os.Exit(1)
 }
 
 // Errorf logs a formatted message at the "error" level. The arguments
